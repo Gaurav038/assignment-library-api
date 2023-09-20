6d1044a608df8bd5e7fa9bad21c3bde687025d6c
GauravSingh <gauravkarki038@gmail.com>
2023-09-20 18:24:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const booksRoutes = require('./routes/books');
+const dotenv = require("dotenv") 
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+dotenv.config()
+
+mongoose.set("strictQuery", false);
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_DB)
+        console.log("mongoDB Connected")
+    } catch (error) {
+        throw error;
+    }
+}
+
+app.use('/api/books', booksRoutes);
+
+app.listen(port, () => {
+    connect()
+  console.log(`Server is running on port ${port}`);
+});
